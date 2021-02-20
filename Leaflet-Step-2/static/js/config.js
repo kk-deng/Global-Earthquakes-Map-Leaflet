3b4cf6ea9a597ab5919e6b7766a8c4d310138047
kk-deng <71301799+kk-deng@users.noreply.github.com>
2021-02-20 00:08:45 -0500

    Updated basic functions

--- Leaflet-Step-2/static/js/config.js
+++ Leaflet-Step-2/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "sk.eyJ1Ijoia2VsdmludDkxMSIsImEiOiJja2tlbmUxNTAwYTNsMnFvOWdxZGprOGk2In0.Nw4QX5CkcKoU1x1jj2YCmQ";
---
+++ Leaflet-Step-2/static/js/logic.js
@@ -0,0 +1,99 @@
+var usaCenter = [44.58, -103.46];
+var mapZoomLevel = 4;
+
+// Create the createMap function
+function createMap(earthquakes) {
+    // Create the tile layer that will be the background of our map
+    var lightLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/light-v10",
+        accessToken: API_KEY
+    });
+
+    var darkLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/dark-v10",
+        accessToken: API_KEY
+    });
+
+    // Create a baseMaps object to hold the multiple layers
+    var baseMaps = {
+        Light: lightLayer,
+        Dark: darkLayer
+    };
+
+    // Create an overlayMaps object to hold the earthquakes layer
+    var overlayMaps = {
+        Earthquakes: earthquakes
+    };
+    // Create the map object with options
+    var map = L.map("mapid", {
+        center: usaCenter,
+        zoom: mapZoomLevel,
+        layers: [lightLayer, earthquakes]
+    });
+
+    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
+};
+
+// Create the createMarkers function
+function createMarkers(earthquakeData) {
+    
+    // function styleLayer(feature) {
+    //     return {
+    //         color: "#fff",
+    //         fillOpacity: 1.0
+    //     }
+    // }
+
+    // // Define a function run for each feature in the features array
+    // function onEachFeature(feature, layer) {
+    //     // Set mouse events to change map styling
+    //     layer.on({
+    //         // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+    //         mouseover: function (event) {
+    //             layer = event.target;
+    //             layer.setStyle({
+    //                 fillOpacity: 0.9
+    //             });
+    //         },
+    //         // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+    //         mouseout: function (event) {
+    //             layer = event.target;
+    //             layer.setStyle({
+    //                 fillOpacity: 0.75
+    //             });
+    //         }
+    //     });
+            
+    //     layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + 
+    //     new Date(feature.properties.time) + "</p>")
+    // }
+
+
+    // var earthquakes = L.geoJson(earthquakeData, {
+    //     style: styleLayer,
+    //     onEachFeature: onEachFeature
+    // })
+    earthquakes = []
+    earthquakeData.forEach(function (feature) {
+        
+    })
+    
+    createMap(earthquakes)
+}
+
+
+var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+d3.json(geoData, data => {
+    console.log(data)
+    createMarkers(data.features)
+})
